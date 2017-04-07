4aa284d1ba49dafea81042eaf67a843d2cac91f7
luxiaogang <xiaogang.lu@oudot.cn>
2017-04-07 15:47:22 +0800

    test

--- app/src/main/java/com/blink/dagger/beziercircle/MainActivity.java
+++ app/src/main/java/com/blink/dagger/beziercircle/MainActivity.java
@@ -2,6 +2,8 @@ package com.blink.dagger.beziercircle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BezierBall button = (BezierBall) findViewById(R.id.jelly);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("luck","is clicked !");
+            }
+        });
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.d("luck","is long clicked !");
+                return false;
+            }
+        });
     }
 }
